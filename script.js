bac6d31ddbf5d1e35d1c814eaf77c73e29720619
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-14 17:19:02 +0200

    added loading-spinner for geolocation

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const searchBtn = document.getElementById("search-btn");
 const searchExitBtn = document.getElementById("search-exit-btn");
 const citiesSearchBtn = document.getElementById("cities-search-btn");
 const favouriteCitiesBtn = document.getElementById("fav-cities-btn");
+const loadingSpinner = document.getElementById("loading-spinner");
 
 const errorMsg = document.createElement("div");
 errorMsg.classList.add("error-msg");
@@ -261,12 +262,19 @@ const getFiveDaysForecast = ({ lat, lon }) => {
 // Function to fetch weather data using geolocation
 const fetchWeatherByGeolocation = () => {
   if (navigator.geolocation) {
+    // Show the loading spinner while waiting for geolocation data
+    loadingSpinner.style.display = "block";
+
     navigator.geolocation.getCurrentPosition((position) => {
+            // Geolocation data is available, so hide the loading spinner
+            loadingSpinner.style.display = "none";
       const { latitude, longitude } = position.coords;
 
       // Fetch weather data using the obtained latitude and longitude
       fetchWeatherDataByGeolocation(latitude, longitude);
     }, (error) => {
+       // Hide the loading spinner in case of errors
+       loadingSpinner.style.display = "none";
       console.error("Geolocation error:", error.message); 
     });
   } else {
